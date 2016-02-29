db8c86d3cef7e1758ea615bb9f106606092e8aff
jmedran <jmedran@gmail.com>
2016-02-28 19:03:44 -0800

    adding city names to search

---
+++ src/containers/weather_list.js
@@ -0,0 +1,40 @@
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+
+  renderWeather(cityData, id) {
+    const cityName = cityData.city.name;
+    return (
+      <tr key={cityName}>
+        <td>{cityName}</td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+            {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps({ weather }) {
+  //const weather = state.weather ^
+  //return { weather: state.weather };
+  return { weather }; // same as ^
+}
+
+export default connect(mapStateToProps)(WeatherList);
