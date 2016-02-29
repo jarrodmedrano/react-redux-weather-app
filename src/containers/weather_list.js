de39b26f797a717ed18dacf852cb13d31d301d3f
jmedran <jmedran@gmail.com>
2016-02-29 09:09:09 -0800

    adding sparklines for temp

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,19 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
-
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 class WeatherList extends Component {
 
   renderWeather(cityData, id) {
     const cityName = cityData.city.name;
+    const cityTemp = cityData.list.map(weather => weather.main.temp);
     return (
       <tr key={cityName}>
         <td>{cityName}</td>
+        <td>
+          <Sparklines height={120} width={180} data={cityTemp}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
